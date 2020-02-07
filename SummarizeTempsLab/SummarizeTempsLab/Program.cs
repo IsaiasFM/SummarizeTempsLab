27253dfa00bfd3e1f1a43cdc90f4bfbecf352952
IsaiasFM <59981709+IsaiasFM@users.noreply.github.com>
2020-02-06 19:17:50 -0600

    Update Program.cs

--- SummarizeTempsLab/SummarizeTempsLab/Program.cs
+++ SummarizeTempsLab/SummarizeTempsLab/Program.cs
@@ -7,7 +7,30 @@ namespace SummarizeTempsLab
     {
         static void Main(string[] args)
         {
+            string FilePath = "temps.txt";
             // temperature data is in temps.txt
+
+
+            //write out prompt to the consoe
+            //Read the filename from the console
+            // Test whether file exists
+
+            //If the file exists
+                //ask the user to enter the temp. threshold
+                //open the file and create StreamReader
+                //read a line into a string variable
+
+                //While the line is not null
+                //Convert (parse) into an integer variable
+                //Add the temp. to my summing variable
+                    //If the current temp value >= theshold
+                        //increment "above" counter by 1
+
+                    //else (temp. is below)
+                        //increment the "below" counter by 1
+                
+                //print out temp. above
+
         }
     }
 }
