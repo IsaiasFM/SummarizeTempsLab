70f49ade683e19bf7aa1dca1b06486ea3936bf9b
IsaiasFM <59981709+IsaiasFM@users.noreply.github.com>
2020-02-11 16:34:59 -0600

    Update Program.cs

--- SummarizeTempsLab/SummarizeTempsLab/Program.cs
+++ SummarizeTempsLab/SummarizeTempsLab/Program.cs
@@ -7,30 +7,78 @@ namespace SummarizeTempsLab
     {
         static void Main(string[] args)
         {
-            string FilePath = "temps.txt";
+            int temp;
+            int SumTemps = 0;
+            int numAbove = 0;
+            int numBelow = 0;
+            int TempCount = 0;
+            string FileName;
             // temperature data is in temps.txt
 
 
-            //write out prompt to the consoe
+            //write out prompt to the console
+            Console.WriteLine("Enter filename.");
+
             //Read the filename from the console
+            FileName = Console.ReadLine();
             // Test whether file exists
 
             //If the file exists
+            if (File.Exists(FileName))
+            {
+                Console.WriteLine("File exists.");
                 //ask the user to enter the temp. threshold
+                Console.WriteLine("Please enter the temperature threshold.");
+                string input;
+                int threshold;
+                input = Console.ReadLine();
+                threshold = int.Parse(input);
+
                 //open the file and create StreamReader
                 //read a line into a string variable
+                using (StreamReader sr = File.OpenText(FileName))
+                {
+                    string line = sr.ReadLine();
 
-                //While the line is not null
-                //Convert (parse) into an integer variable
-                //Add the temp. to my summing variable
-                    //If the current temp value >= theshold
-                        //increment "above" counter by 1
+                    //While the line is not null
+                    while (line != null)
+                    {
+                        //Convert (parse) into an integer variable
+                        temp = int.Parse(line);
+                       
+                        //Add the temp. to my summing variable
+                        SumTemps += temp;
 
-                    //else (temp. is below)
-                        //increment the "below" counter by 1
-                
-                //print out temp. above
+                        TempCount += 1;
 
+                        //If the current temp value >= theshold
+                        if (temp >= threshold)
+                        {
+                            //increment "above" counter by 1
+                           numAbove += 1;
+                        }
+
+                        //else (temp. is below)
+                        else
+                        {
+                           //increment the "below" counter by 1
+                           numBelow += 1;
+                        }
+
+                        line = sr.ReadLine();
+                    }
+                    Console.WriteLine("Temperatures above = " + numAbove);
+                    Console.WriteLine("temperatures below = " + numBelow);
+                    int average = SumTemps / TempCount;
+
+                    Console.WriteLine("Average Temp = " + average);
+                }
+                //Print out Temperaturs above the threshold
+            }
+            else
+            {
+                Console.WriteLine("file does not exists.");
+            }
         }
     }
 }
