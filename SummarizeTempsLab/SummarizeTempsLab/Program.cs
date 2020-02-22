896c9e3bfb3a84b26a1eb7a362038d64d501cd53
IsaiasFM <59981709+IsaiasFM@users.noreply.github.com>
2020-02-22 11:24:36 -0600

    Update Program.cs

--- SummarizeTempsLab/SummarizeTempsLab/Program.cs
+++ SummarizeTempsLab/SummarizeTempsLab/Program.cs
@@ -9,18 +9,54 @@ namespace SummarizeTempsLab
         {
             string FileName;
 
+            Console.WriteLine("Please enter the file name.");
+
             FileName = Console.ReadLine();
 
             if (File.Exists(FileName))
             {
-                int UserInput;
+                int TempInput;
 
                 Console.WriteLine("File exists.");
                 Console.WriteLine("Please enter a temperature threshold.");
 
-                UserInput = int.Parse(Console.ReadLine());
+                TempInput = int.Parse(Console.ReadLine());
+
                 using (StreamReader sr = File.OpenText(FileName))
                 {
+                    string line = sr.ReadLine();
+                    int Temp;
+                    int SumTemps = 0;
+                    int TempCount = 0;
+                    int TempAbove = 0;
+                    int TempBelow = 0;
+                    int AverageTemp;
+
+                    while (line != null)
+                    {
+                        Temp = int.Parse(line);
+
+                        SumTemps += Temp;
+
+                        TempCount += 1;
+
+                        if (Temp >= TempInput)
+                        {
+                            TempAbove += 1;
+                        }
+
+                        else
+                        {
+                            TempBelow += 1;
+                        }
+                    }
+
+                    AverageTemp = SumTemps / TempCount;
+
+                    Console.WriteLine("Number of temperatures above the input threshold: " + TempAbove);
+                    Console.WriteLine("Number of temperatures below the input threshold: " + TempBelow);
+                    Console.WriteLine("Average temperature: " + AverageTemp);
+
 
                 }
             }
